9468a92cac57d328b3566f5075a37dbdac3c5d56
Alexander Kostrovsky <alkstr@mail.ru>
2024-05-04 21:46:11 +0500

    add general component for sidebar tabs

---
+++ src/components/Map/Sidebar/SidebarTab.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import { SidebarState, useMapState } from "../MapStateProvider";
+
+interface SidebarTabProps {
+    state: SidebarState;
+    header: String;
+    children: ReactNode;
+}
+
+export const SidebarTab = (props: SidebarTabProps) => {
+    const { sidebarState, isSidebarVisible } = useMapState();
+    return (
+        <div className={"absolute w-96 h-full p-4 bg-white border-r-2 border-gray-300 flex flex-col gap-2 " + (sidebarState == props.state && isSidebarVisible ? "" : "-translate-x-full")} style={{zIndex: 1}}>
+            <div className="border-b-2 text-2xl font-semibold">{props.header}</div>
+            {props.children}
+        </div>
+    )
+}
