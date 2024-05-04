c96b5c5e47c4d720c7d9cb98d50f4517398abbb7
Alexander Kostrovsky <alkstr@mail.ru>
2024-05-04 20:21:16 +0500

    fix filters clearing when sidebar hides

--- src/components/Map/Sidebar/Sidebar.tsx
+++ src/components/Map/Sidebar/Sidebar.tsx
@@ -10,26 +10,26 @@ export const Sidebar = () => {
     }
 
     return (
-        isSidebarVisible ?
-            <div>
-                <div className="absolute w-96 h-full p-4 bg-white border-r-2 border-gray-300 " style={{ zIndex: 1 }}>
-                    {stateMap[sidebarState]}
-                </div>
-                <button
-                    className="absolute ml-96 mt-2 text-5xl"
-                    style={{ zIndex: 2 }}
-                    onClick={() => setIsSidebarVisible(false)}>
-                    ⬅️
-                </button>
-            </div> :
-            <div>
-                <button
-                    className="absolute ml-2 mt-2 text-5xl"
-                    style={{ zIndex: 2 }}
-                    onClick={() => setIsSidebarVisible(true)}>
-                    ➡️
-                </button>
+        <div>
+            <div className={"absolute w-96 h-full p-4 bg-white border-r-2 border-gray-300 " + (isSidebarVisible ? "" : "-translate-x-full")} style={{ zIndex: 1 }}>
+                {stateMap[sidebarState]}
             </div>
+            {
+                isSidebarVisible ?
+                    <button
+                        className="absolute ml-96 mt-2 text-5xl"
+                        style={{ zIndex: 2 }}
+                        onClick={() => setIsSidebarVisible(false)}>
+                        ⬅️
+                    </button> :
+                    <button
+                        className="absolute mt-2 text-5xl"
+                        style={{ zIndex: 2 }}
+                        onClick={() => setIsSidebarVisible(true)}>
+                        ➡️
+                    </button>
+            }
+        </div>
     )
 }
 
