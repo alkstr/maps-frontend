c32c4b48e9410d5316ae6d068af696782d110eb7
Alexander Kostrovsky <alkstr@mail.ru>
2024-05-15 01:41:58 +0500

    fix format

--- src/components/Map/Sidebar/SidebarTab.tsx
+++ src/components/Map/Sidebar/SidebarTab.tsx
@@ -10,7 +10,9 @@ interface SidebarTabProps {
 export const SidebarTab = (props: SidebarTabProps) => {
     const { sidebarState, isSidebarVisible } = useMapState();
     return (
-        <div className={"absolute w-96 h-full p-4 bg-white border-r-2 border-gray-300 flex flex-col gap-2 " + (sidebarState == props.state && isSidebarVisible ? "" : "-translate-x-full")} style={{zIndex: 1}}>
+        <div className={"absolute w-96 h-full p-4 bg-white border-r-2 border-gray-300 flex flex-col gap-2 overflow-y-auto " +
+            (sidebarState == props.state && isSidebarVisible ? "" : "-translate-x-full")}
+            style={{ zIndex: 1 }}>
             <div className="border-b-2 text-2xl font-semibold">{props.header}</div>
             {props.children}
         </div>
