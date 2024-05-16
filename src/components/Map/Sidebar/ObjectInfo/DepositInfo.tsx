81e868c000af9004e4126e55856e9ae632e0130f
Alexander Kostrovsky <alkstr@mail.ru>
2024-05-16 08:49:22 +0500

    fix component structure

--- src/components/Map/Sidebar/ObjectInfo/DepositInfo.tsx
+++ src/components/Map/Sidebar/ObjectInfo/DepositInfo.tsx
@@ -3,10 +3,13 @@ import { Deposit } from "@/domain";
 export const DepositInfo = (props: { deposit: Deposit }) => {
     return (
         <div>
-            <h3 className="text-lg border-b-2">Название</h3>
-            {props.deposit.name}
-            <h3 className="text-lg border-b-2">Код ОКАТО</h3>
-            {props.deposit.okato_code}
+            <h2 className="text-xl font-medium">Месторождение</h2>
+            <div className="mt-2 mr-4 p-2 rounded border-2">
+                <h3 className="text-lg border-b-2">Название</h3>
+                {props.deposit.name}
+                <h3 className="text-lg border-b-2">Код ОКАТО</h3>
+                {props.deposit.okato_code}
+            </div>
         </div>
     );
 }
