3024a91cb750cdb1293a3815403b60a055cb0050
Alexander Kostrovsky <alkstr@mail.ru>
2024-05-17 13:05:39 +0500

    fix styling

--- src/components/Map/Sidebar/SidebarTab.tsx
+++ src/components/Map/Sidebar/SidebarTab.tsx
@@ -13,7 +13,7 @@ export const SidebarTab = (props: SidebarTabProps) => {
         <div className={"absolute w-96 h-full p-4 bg-white border-r-2 border-gray-300 flex flex-col gap-2 overflow-y-auto " +
             (sidebarState == props.state && isSidebarVisible ? "" : "-translate-x-full")}
             style={{ zIndex: 1 }}>
-            <div className="border-b-2 text-2xl font-semibold">{props.header}</div>
+            <div className="border-b-2 mr-12 text-2xl font-semibold">{props.header}</div>
             {props.children}
         </div>
     );
