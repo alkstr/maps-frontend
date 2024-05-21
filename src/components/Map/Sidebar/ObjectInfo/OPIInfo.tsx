9718a0770d0e4aecbf293a1e69835fc9832ee8d0
Alexander Kostrovsky <alkstr@mail.ru>
2024-05-21 17:01:24 +0500

    add numeration to a list

--- src/components/Map/Sidebar/ObjectInfo/OPIInfo.tsx
+++ src/components/Map/Sidebar/ObjectInfo/OPIInfo.tsx
@@ -11,20 +11,17 @@ export const OPIInfo = (props: OPIInfoProps) => {
             {
                 props.opiList.length == 0 ?
                     <div className="mt-2 mr-4 p-2 rounded border-2">На данном участке ничего не добывается</div> :
-                    props.opiList.map((o: OPI, i: number) => <OPICard key={i} opi={o} />)
+                    props.opiList.map((o: OPI, i: number) => <OPICard key={i} number={i} opi={o} />)
             }
         </div>
     );
 }
 
-interface OPICardProps {
-    opi: OPI;
-}
-
-const OPICard = (props: OPICardProps) => {
+const OPICard = (props: { opi: OPI, number: number}) => {
     return (
         <div className="mt-2 mr-4 p-2 rounded border-2">
-            <h3 className="text-lg border-b-2">Название</h3>
+            <div className="absolute right-10 text-gray-500">{`#${props.number + 1}`}</div>
+            <h3 className="text-lg border-b-2">Вид</h3>
             {props.opi.name}
             <h3 className="text-lg border-b-2">Код</h3>
             {props.opi.code}
