8ecda9a3fe291ff675847697ebee5783c7b658c0
Alexander Kostrovsky <alkstr@mail.ru>
2024-05-22 12:20:32 +0500

    fix styling

--- src/components/Map/Sidebar/ObjectInfo/ObjectInfo.tsx
+++ src/components/Map/Sidebar/ObjectInfo/ObjectInfo.tsx
@@ -30,7 +30,10 @@ const CloseButton = () => {
             className="absolute right-4 top-4 border-2 border-gray-300 rounded drop-shadow-sm select-none 
             hover:bg-gray-200 active:bg-gray-300"
             onClick={() => setSidebarState(SidebarState.Filters)}>
-            ❌
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" className="size-6">
+                <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+            </svg>
+
         </button>
     );
 }
