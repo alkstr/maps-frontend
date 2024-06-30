6dcc60fc21e886de0f0bc4bfb22bd51ef5660347
Alexander Kostrovsky <alkstr@mail.ru>
2024-06-30 22:53:17 +0500

    change fetching to mock

--- src/api.ts
+++ src/api.ts
@@ -35,19 +35,19 @@ function parse(obj: areasGetSchema): Area {
     )
 }
 
+// export async function fetchAreas(params: areasGetParams =
+//     { is_not_license: "", opi: "", deposit_name: "", okato_name: "" }):
+//     Promise<Area[]> {
+//     const queryParams = new URLSearchParams(params);
+//     const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/area/?${queryParams}`, {
+//         method: "GET",
+//     });
+
+//     const data = await response.json();
+//     return data.map((obj: areasGetSchema) => parse(obj));
+// }
+
 export async function fetchAreas(params: areasGetParams =
-    { is_not_license: "", opi: "", deposit_name: "", okato_name: "" }):
-    Promise<Area[]> {
-    const queryParams = new URLSearchParams(params);
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/area/?${queryParams}`, {
-        method: "GET",
-    });
-
-    const data = await response.json();
-    return data.map((obj: areasGetSchema) => parse(obj));
-}
-
-export async function fetchAreasMock(params: areasGetParams =
     { is_not_license: "", opi: "", deposit_name: "", okato_name: "" }):
     Promise<Area[]> {
     const data = MOCK_DATA.filter((obj: areasGetSchema) => {
@@ -71,5 +71,24 @@ export async function fetchAreasMock(params: areasGetParams =
 }
 
 const MOCK_DATA: areasGetSchema[] = [
-
+    {
+        name: "Мостовский участок",
+        coordinates: [
+            { long: "57.285686", lat: "60.435451" },
+            { long: "57.267688", lat: "60.472297" },
+            { long: "57.255646", lat: "60.436122" },
+            { long: "57.283783", lat: "60.410412" },
+        ],
+        deposit: { name: "Мостовское торфяное месторождение", okato: "65484000007" },
+        owners: [{ name: "Иванов Иван Иванович", address: "?", registration: "5/25/2020", end_date: "5/25/2025" }],
+        opi: [
+            { code: 1030, name: "Торф" },
+            { code: 10301, name: "Торф, используемый в лечебных целях" },
+        ],
+        category_a: 10000,
+        category_b: 5000,
+        category_c1: 2000,
+        category_c2: 5000,
+        year_estimation: "5/25/2020",
+    }
 ];
